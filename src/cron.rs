fc6ed896bf23e2da798e890ae73797852b71304d
Thibault Tisserand <40698142+gzordrai@users.noreply.github.com>
2025-11-24 14:49:14 +0100

    feat: add task to cron

--- src/cron.rs
+++ src/cron.rs
@@ -7,7 +7,11 @@ pub struct Cron {
 
 impl Cron {
     pub fn new() -> Self {
-        Self { tasks: vec![] }
+        Self::default()
+    }
+
+    pub fn add_task(&mut self, task: CronTask) {
+        self.tasks.push(task);
     }
 }
 
